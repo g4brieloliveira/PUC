e06ec004f5830293727024e4a5b102c72b564831
g4brieloliveira <g4brieloliveira.dev@gmail.com>
2022-08-25 20:10:20 -0300

    java exercise

--- semester_01/ATP/aula24_02/Exercicio1.java
+++ semester_01/ATP/aula24_02/Exercicio1.java
@@ -74,7 +74,7 @@
 
 //     input.close();
 
-//     value = (pressure * volume / (temperature + 460)) * 0.37;
+//     value = (pressure * volume / (temperature + 460)) / 0.37;
 
 //     System.out.println("A massa de ar desse pneu é: " + value);
 //   }
@@ -83,36 +83,151 @@
 // 4. Escreva um programa que pergunte qual o tempo transcorrido em um cronômetro em 
 // horas, minutos e segundos e transforme (e mostre) todo este tempo em segundos. 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     int hours, minutes, seconds, value;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite o valor das horas: ");
+//     hours = input.nextInt();
+//     System.out.println("Digite o valor dos minutos: ");
+//     minutes = input.nextInt();
+//     System.out.println("Digite o valor dos segundos: ");
+//     seconds = input.nextInt();
+
+//     input.close();
+
+//     value = (hours * 3600) + (minutes * 60) + seconds;
+
+//     System.out.println("Foram percorridos " + value + " segundos no total");
+//   }
+// }
 
 // 5. Escreva um programa que leia uma temperatura em graus Farenheit e a imprima em graus 
 // Centígrados. A conversão de graus Farenheit para Centígrados é obtida por C=(5/9)(F-32) 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     double farenheit, celsius;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite a temperatura em graus Farenheit: ");
+//     farenheit = input.nextFloat();
+//     input.close();
+
+//     celsius = (farenheit - 32) / 1.8;
+
+//     System.out.println(farenheit + "°F é igual a " + celsius + "°C");
+//   }
+// }
 
 // 6. Determinar a quantidade de litros de combustível gastos em uma viagem por um 
 // automóvel que faz 12km/l. Para isso, sabe-se que o tempo gasto na viagem é t = 35min e 
 // a velocidade média do automóvel é v = 80km/h. 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     float consumption = 12, time = 35, velocity = 80, value;
+
+//     value = (((time / 60) * velocity) / consumption);
+
+//     System.out.println("Foram gastos " + value + " litros de combustível");
+//   }
+// }
 
 // 7. Criar um algoritmo que obtenha 4 números reais e exiba a sua média ponderada, sabendo-
 // se que os pesos são 2, 1, 3 e 4, nesta ordem. 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     float num1, num2, num3, num4, value;
+//     int p1 = 2, p2 = 1, p3 = 3, p4 = 4;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite o primeiro numero: ");
+//     num1 = input.nextInt();
+//     System.out.println("Digite o segundo numero: ");
+//     num2 = input.nextInt();
+//     System.out.println("Digite o terceiro numero: ");
+//     num3 = input.nextInt();
+//     System.out.println("Digite o quarto numero: ");
+//     num4 = input.nextInt();
+//     input.close();
+
+//     value = ((num1 * p1) + (num2 * p2) + (num3 * p3) + (num4 * p4)) / (p1 + p2 + p3 + p4);
+
+//     System.out.println("Media: " + value);
+//   }
+// }
 
 // 8. Para vários tributos a base de calculo é o salário mínimo. Fazer um algoritmo que leia o o 
 // valor do salário de uma pessoa. Calcular e exibir quantos salários mínimos essa pessoa 
 // ganha. Considere que o valor do salário mínimo é: R$1.212,00. 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     float salary, value;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite seu salario: ");
+//     salary = input.nextInt();
+//     input.close();
+
+//     value = salary / 1212;
+
+//     System.out.println("Este salário equivale a " + value + " salários mínimos");
+//   }
+// }
 
 // 9. Todo restaurante cobra 10% sobre o valor do consumo do cliente para o garçom, embora 
 // por lei não possa obrigar o cliente a pagar. Fazer um algoritmo que leia o valor gasto com 
 // as despesas realizadas em um restaurante e imprima o valor da gorjeta e o valor total a 
 // ser pago (despesa mais gorjeta). 
  
+// import java.util.*;
 
+// class Main {
+//   public static void main(String[] args) {
+//     double consumption, tip, value;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite o consumo: ");
+//     consumption = input.nextInt();
+//     input.close();
+
+//     tip = consumption * 0.1;
+//     value = consumption + tip;
+
+//     System.out.println("O valor da gorjeta é: R$" + tip + "\nO consumo mais a gorjeta fica igual a: R$" + value);
+//   }
+// }
  
 // 10. Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar todos os 
 // cavalos comprados para um haras 
+
+// import java.util.*;
+
+// class Main {
+//   public static void main(String[] args) {
+//     int horses, value;
+
+//     Scanner input = new Scanner(System.in);
+//     System.out.println("Digite a quantidade de cavalos comprada: ");
+//     horses = input.nextInt();
+//     input.close();
+
+//     value = (horses * 4);
+
+//     System.out.println("Deverá comprar " + value + " ferraduras");
+//   }
+// }
